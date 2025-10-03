79f709d69cb6c277d12206ca1eb14fbf237085bd
gabby <hannahfrench1709@hotmail.com>
2025-10-03 13:16:41 +0100

    feat: add DeleteButton and set up functionality

---
+++ src/components/deletebutton/DeleteButton.jsx
@@ -0,0 +1,24 @@
+import { db } from "@/utils/database-connection";
+import { revalidatePath } from "next/cache";
+
+export default function DeleteButton({ commentId, pokemonId }) {
+
+    async function handleDelete(formData) {
+        "use server";
+
+        const id = formData.get("id");
+        console.log(id);
+
+        db.query(
+                    `DELETE FROM comments WHERE id = $1`, [id]
+                )
+        revalidatePath(`/pokemons/${pokemonId}`);
+    }
+
+    return (
+        <form action={handleDelete}>
+            <input type="hidden" name="id" value={commentId}/>
+            <button type="submit">Delete</button>
+        </form>
+    )
+}
