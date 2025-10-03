b4eca92c8ecd5ee8715ec3c16ba186139cb49de5
gabby <hannahfrench1709@hotmail.com>
2025-10-03 13:20:02 +0100

    feat: update file path for Header component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import Header from "@/components/Header";
+import Header from "@/components/header/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
