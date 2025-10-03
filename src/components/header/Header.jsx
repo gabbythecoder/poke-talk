50e3872ea60fc7c00e215d936fdaae7e2336e33c
gabby <hannahfrench1709@hotmail.com>
2025-10-03 18:04:13 +0100

    style: add styling to Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,33 @@
+"use client";
+
 import Link from "next/link";
+import Image from "next/image";
+import HeaderLogo from "@/../public/images/poketalk-logo.png";
+import headerStyle from "./Header.module.css";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+    const pathname = usePathname();
+    
     return (
-        <header>
-            <h1>PokéTalk</h1>
+        <header className={headerStyle.header}>
 
-            <nav>
-                <Link href={"/"}>Home</Link>
-                <Link href={"/pokemons"}>Pokémons</Link>
+            <div className={headerStyle.headerContainer}>
+
+            <Image 
+                src={HeaderLogo}
+                alt="poketalk logo"
+                className={headerStyle.logoImage}
+            />
+
+            <nav className={headerStyle.navBar}>
+                <Link className={
+                    pathname === "/" ? `${headerStyle.navLink} ${headerStyle.active}` : headerStyle.navLink} href={"/"}>HOME</Link>
+                <Link className={
+                            pathname.includes("/pokemons") ? `${headerStyle.navLink} ${headerStyle.active}` : headerStyle.navLink} href={"/pokemons"}>POKÉMON</Link>
             </nav>
+
+            </div>
         </header>
     )
 }
